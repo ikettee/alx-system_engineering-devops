d81236a55055e993f6e21f74853b55e047fccf3b
Isaac Gama <ikettee@gmail.com>
2024-03-23 22:20:55 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Install specific versions of Flask and Werkzeug
+package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
